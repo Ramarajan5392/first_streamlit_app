62b648c0eac7515dd72c7e452fc04720806fe64d
Ramarajan <106595501+Ramarajan5392@users.noreply.github.com>
2022-06-01 11:23:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit;
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.subheader('Omega 3 & Blueberry Oat Meal')
-
+streamlit.subheader('Spinach & Smoothie')
